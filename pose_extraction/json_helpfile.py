b26bf94856a5d8a2b82e9d294ced5fcf7910cd3d
Isak Engström <isaeng@K2006560.win.foi.se>
2021-02-25 17:13:56 +0100

    Add json_helpfile.py

---
+++ pose_extraction/json_helpfile.py
@@ -0,0 +1,42 @@
+import json
+import numpy as np
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """
+    Special json encoder for numpy types
+
+    Original code from: https://github.com/mpld3/mpld3/issues/434#issuecomment-340255689
+
+    Then modified by: https://stackoverflow.com/a/49677241
+
+    """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def save_to_json(dic, target_dir):
+    """
+    A function that saves JSON files.
+    """
+    dumped = json.dumps(dic, cls=NumpyEncoder)
+    file = open(target_dir, 'w')
+    json.dump(dumped, file)
+    file.close()
+
+
+def read_from_json(target_dir):
+    """
+    A function that reads JSON files.
+    """
+    f = open(target_dir, 'r')
+    data = json.load(f)
+    data = json.loads(data)
+    f.close()
+    return data
