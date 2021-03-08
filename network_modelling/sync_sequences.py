d3363cae8ae6aecaa061253f5990a21bb8a79216
Isak Engström <isaeng@K2006560.win.foi.se>
2021-03-08 10:44:42 +0100

    Init frame synchronisation
    - Test to see if the frames in the json files can be synced.
    
    - The idea is to use the syncing audio of the original clips.

---
+++ network_modelling/sync_sequences.py
@@ -0,0 +1,15 @@
+"""
+http://www.dsg-bielefeld.de/dsg_wp/wp-content/uploads/2014/10/video_syncing_fun.pdf
+"""
+
+import os
+import subprocess
+import glob
+
+
+def synchronise():
+    """"""
+
+
+if __name__ == "__main__":
+    synchronise()
