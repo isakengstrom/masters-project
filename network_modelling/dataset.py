4f7a02f80b984986bb589822e233460361b55e02
Isak Engström <isaeng@K2006560.win.foi.se>
2021-03-25 14:33:16 +0100

    Create lookup table
    - The lookup table keeps track of the sequence intervals and other neccessary info. The table is a list where every element is a dictionary. Every dictionary contains the file, subject, session and view names, as well as starting and ending frame for that particular sequence.

--- network_modelling/dataset.py
+++ network_modelling/dataset.py
@@ -2,18 +2,83 @@ import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+#import pandas as pd
+import os
 
-from helpers.paths import OP_EXTRACTED_PATH
+from helpers import read_from_json
+
+
+class DatasetElement:
+    def __init__(self, element):
+        self.file_name = element["file_name"]
+        self.sub_name = element["sub_name"]
+        self.sess_name = element["sess_name"]
+        self.view_name = element["view_name"]
+
+        self.sub = int(self.sub_name[-1])
+        self.sess = int(self.sess_name[-1])
+        self.view = int(self.view_name[-1])
+
+
+class InfoElement(DatasetElement):
+    def __init__(self, element):
+        super().__init__(element)
+
+        self.len = element["len"]
+        self.shape = element["shape"]
+
+
+class SeqElement(DatasetElement):
+    def __init__(self, element):
+        super().__init__(element)
+
+        self.start = int(element["start"])
+        self.end = int(element["end"])
+
 
 class FOIKineticPoseDataset(Dataset):
-    def __init__(self, json_path, root_dir, transform=None):
+    def __init__(self, json_path, root_dir, sequence_len, transform=None):
         # Data loading
-        print()
+        data_info = read_from_json(json_path)
+        print(data_info)
 
-    def __getitem__(self, index):
-        # Dataset[0]
-        print()
+        lookup = []
+
+        for element in data_info:
+            el = InfoElement(element)
+            #print("File name {}, len {}, shape {}".format(el.file_name, el.len, el.shape))
+
+            seq_info = dict()
+            seq_info["file_name"] = el.file_name
+            seq_info["sub_name"] = el.sub_name
+            seq_info["sess_name"] = el.sess_name
+            seq_info["view_name"] = el.view_name
+
+            for i in range(0, el.len, sequence_len):
+                seq_info["start"] = i
+                seq_info["end"] = i + sequence_len - 1
+                lookup.append(seq_info.copy())
+
+            # Fix the last sequence's end frame
+            lookup[-1]["end"] = min(lookup[-1]["end"], el.len)
+
+        self.lookup = lookup
+        self.root_dir = root_dir
+        self.transform = transform
+        print("Init")
 
     def __len__(self):
-        # len(dataset)
-        print()
+        return len(self.lookup)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        seq_name = os.path.join(self.root_dir, self.lookup.iloc[idx, 0])
+
+        print("Get item")
+
+
+
+
+
--- network_modelling/main.py
+++ network_modelling/main.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 import torchvision.transforms as transforms
-
 from models.LSTM import LSTM
 from train import train
 from test import test
+from dataset import FOIKineticPoseDataset
 
+from helpers.paths import EXTR_PATH
 
 
 
@@ -15,18 +17,24 @@ if __name__ == "__main__":
     # Hyper parameters:
     hidden_size = 128
     num_classes = 10
-    start_epochs = 1
+    start_epoch = 1
     num_epochs = 2
     batch_size = 100
     learning_rate = 0.001
 
     input_size = 28
-    sequence_length = 28
+    sequence_len = 100
     num_layers = 2
+
+    # Other params
+    json_path = EXTR_PATH + "final_data_info.json"
+    root_dir = EXTR_PATH + "final/"
+
 
     use_cuda = torch.cuda.is_available()
 
-    trainset = torchvision.datasets.CIFAR10
+    kinetic_dataset = FOIKineticPoseDataset(json_path, root_dir, sequence_len)
+    #train_loader = DataLoader(train_set, batch_size, shuffle=True, num_workers=2)
 
     model = LSTM(input_size, hidden_size, num_layers, num_classes)
 
@@ -39,5 +47,5 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters())
 
 
-    model, loss_log, acc_log = train(model, trainloader, optimizer, objective, use_cuda, start_epoch, num_epochs)
+    #model, loss_log, acc_log = train(model, train_loader, optimizer, objective, use_cuda, start_epoch, num_epochs)
 
