b70e4853d7d6a3e311189d9cbbdc57e265aa8603
Isak Engström <isaeng@K2006560.win.foi.se>
2021-03-25 08:25:29 +0100

    Clean up paths
    - Clean needed, as too many paths where being created. Removed some paths and will instead add dirs to the paths directly in the code if it is required to add some dir to the paths.

--- helpers/paths.py
+++ helpers/paths.py
@@ -1,20 +1,21 @@
 import os
 
+# Naming Conventions:
+# OP - OpenPose
+# EXTR - Extracted
+# KP - KeyPoints
+# CC - Cross Correlation
+
+# Unused: UT - untrimmed # T - trimmed # F - final
+
 # Path to the foi dataset
 DATASET_PATH = os.environ['DATASET_DIR'] + "VIDEO/"  # Path to the dataset that should be extracted from
 
 # dir to save the openpose keypoints at
 OP_SAVE_PATH = os.environ['DATASET_DIR'] + "/isaeng_extr/json_dumps_test"
 
-# Name of folders used in paths below
-SYNC_STATUS = "non_synced"  # non_synced / synced
-TRIM_STATUS = "untrimmed_version"  # untrimmed_version / trimmed_version
-
-# Paths to fetch the openpose keypoints from
-OP_EXTRACTED_PATH = os.environ['DATASET_DIR'] + "EXTRACTED_KEYPOINTS/isaeng/{}/{}/".format(TRIM_STATUS, SYNC_STATUS)
-
-# Path to fetch/save the synced openpose keypoints from
-OP_SYNCED_PATH = os.environ['DATASET_DIR'] + "EXTRACTED_KEYPOINTS/isaeng/{}/synced/".format(TRIM_STATUS)
+# Paths to the openpose keypoints
+EXTR_PATH = os.environ['DATASET_DIR'] + "EXTRACTED_KEYPOINTS/isaeng/"
 
 # Path to the json that stores at which frames the untrimmed extracted
 TRIM_INTERVALS = os.environ['PROJECT_PATH'] + "users/isaeng/data_processing/pre_processing/frame_intervals.json"
