2647145ae49734760549954c1904734c34949c78
Isak Engström <isaeng@K2006560.win.foi.se>
2021-03-26 15:35:11 +0100

    Add path to joints lookup table

--- helpers/paths.py
+++ helpers/paths.py
@@ -20,5 +20,8 @@ EXTR_PATH = os.environ['DATASET_DIR'] + "EXTRACTED_KEYPOINTS/isaeng/"
 # Path to the json that stores at which frames the untrimmed extracted
 TRIM_INTERVALS = os.environ['PROJECT_PATH'] + "users/isaeng/data_processing/pre_processing/frame_intervals.json"
 
+# Path to the joints lookup table
+JOINTS_LOOKUP_PATH = os.environ['PROJECT_PATH'] + "users/isaeng/network_modelling/joints_lookup.json"
+
 # Path to the session offsets extracted using cross correlation
 CC_OFFSETS_PATH = os.environ['DATASET_DIR'] + "/isaeng_extr/json_dumps_test/offsets.json"
