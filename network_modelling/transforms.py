763111ba512734dcb38fc04557a6a15edd0571f2
Isak Engström <isaeng@K2006560.win.foi.se>
2021-03-31 15:04:36 +0200

    Add comments to the 'NormalisePoses' transform

--- network_modelling/transforms.py
+++ network_modelling/transforms.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from helpers import read_from_json
 from helpers.paths import JOINTS_LOOKUP_PATH
-import time
+
 
 class ToTensor:
     """
@@ -20,6 +20,11 @@ class ChangePoseOrigin(object):
 
 
 class NormalisePoses(object):
+    """
+    Transform which normalises the poses in a sequence in a given range, default being [0, 1].
+    The normalisation is applied locally on each pose, compared to applying it globally to a sequence.
+    """
+
     def __init__(self, low=0, high=1):
         self.low = low
         self.high = high
@@ -29,38 +34,36 @@ class NormalisePoses(object):
         seq_id, seq = item["id"], item["sequence"]
         assert type(seq) is np.ndarray
 
+        # Get the min and max of the x and y coordinates separately for each pose in the sequence.
+        # The vars are of shape (sequence_len,)
         min_x = np.amin(seq[:, :, 0], axis=1)
         max_x = np.amax(seq[:, :, 0], axis=1)
         min_y = np.amin(seq[:, :, 1], axis=1)
         max_y = np.amax(seq[:, :, 1], axis=1)
 
-        max_dist_x = np.abs(max_x - min_x)
-        max_dist_y = np.abs(max_y - min_y)
+        # Calculate the distance for each dimension in which the coordinates lie.
+        dist_x = np.abs(max_x - min_x)
+        dist_y = np.abs(max_y - min_y)
 
-        condition = max_dist_x > max_dist_y
-        tile_len = seq.shape[1] * seq.shape[2]
-
+        # Pick the min and distance to use for each pose in the sequence depending on the condition.
+        # The condition checks which distance is larger of the x and y dimension.
+        # It should normalise along the largest dimensional distance.
+        condition = dist_x > dist_y
         seq_mins = np.where(condition, min_x, min_y)
-        seq_mins = np.tile(seq_mins, tile_len).reshape(seq.shape, order='F')
+        seq_dists = np.where(condition, dist_x, dist_y)
 
-        seq_denoms = np.where(condition, max_dist_x, max_dist_y)
-        seq_denoms = np.tile(seq_denoms, tile_len).reshape(seq.shape, order='F')
+        # Tiles the vars from shape (seq_len,) to (seq_len, num_openpose_joints, coords), most likely (seq_len, 25, 2)
+        # The values are unchanged, but need to be broadcast to the same dimensions as the input sequence for the
+        # next step.
+        tile_len = seq.shape[1] * seq.shape[2]
+        seq_mins = np.tile(seq_mins, tile_len).reshape(seq.shape, order='F')
+        seq_dists = np.tile(seq_dists, tile_len).reshape(seq.shape, order='F')
 
         # Normalise to interval [0, 1]
-        normalised = (seq - seq_mins) / seq_denoms
+        normalised = (seq - seq_mins) / seq_dists
 
         # Normalise from [0,1] to [self.low, self.high]
         normalised = normalised * (self.high - self.low) + self.low
-
-        '''
-        for pose_idx in range(len(seq_mins)):
-            if pose_idx < 1000:
-                print("----------------------------------------------------------")
-                print(min(normalised[pose_idx, :, 0]), max(normalised[pose_idx, :, 0]))
-                print(min(normalised[pose_idx, :, 1]), max(normalised[pose_idx, :, 1]))
-                # time.sleep(1)
-
-        '''
 
         return {"id": seq_id, "sequence": normalised}
 
