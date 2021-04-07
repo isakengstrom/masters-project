059359fb221c76579841c1fa3de7e393c8c6a0cd
Isak Engström <isaeng@K2006560.win.foi.se>
2021-04-07 17:12:35 +0200

    Remove .do(device) and use_cuda
    - This is already handeled in the main. Still add a TODO to make sure that it works the way it's supposed to.

--- network_modelling/models/LSTM.py
+++ network_modelling/models/LSTM.py
@@ -7,18 +7,18 @@ import torch.nn as nn
 # https://towardsdatascience.com/metric-learning-loss-functions-5b67b3da99a5
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, use_cuda):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(LSTM, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
 
         self.fc = nn.Linear(hidden_size, num_classes)
-        self.device = torch.device('cuda' if use_cuda else 'cpu')
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        # TODO: Make sure this is passed to device
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
 
         out, _ = self.lstm(x, (h0, c0))
 
