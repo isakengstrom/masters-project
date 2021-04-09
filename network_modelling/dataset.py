223525d2c4dbee22768356f7c96ca0bc1c493517
Isak Engström <isaeng@K2006560.win.foi.se>
2021-04-09 15:46:04 +0200

    Begin adapt dataset to network types
    - Network types can be single, siamese and triplet.
    
    - Also, add a class object of Sequences, which will use the __call__ method, instead of creating new instances of the Sequence class every time

--- network_modelling/dataset.py
+++ network_modelling/dataset.py
@@ -92,26 +92,47 @@ class Sequence:
         file_data = read_from_json(file_path)
 
         seq_data = file_data[seq_info.start:seq_info.end]
-        seq_data = np.array(seq_data)
-
         self.poses = np.array(seq_data)
 
     def __len__(self):
         return self.__sequence_len
 
     def __getitem__(self, idx):
+        print("Getting a sequence item")
         return self.poses[idx]
 
 
+class Sequences:
+    def __init__(self, root_dir, sequence_len):
+        self.__root_dir = root_dir
+        self.__sequence_len = sequence_len
+
+    def __len__(self):
+        return self.__sequence_len
+
+    def __call__(self, item):
+        seq_info = SequenceElement(item)
+
+        file_path = os.path.join(self.__root_dir, seq_info.file_name)
+        file_data = read_from_json(file_path)
+
+        seq_data = file_data[seq_info.start:seq_info.end]
+
+        return np.array(seq_data), seq_info.label
+
+
+# TODO: get positives and negatives correctly, they are all the same sequence atm
 class FOIKineticPoseDataset(Dataset):
-    def __init__(self, json_path, root_dir, sequence_len, data_limiter=None, transform=None):
+    def __init__(self, json_path, root_dir, sequence_len, network_type="triplet", data_limiter=None, transform=None):
         # Data loading
         self.json_path = json_path
-        self.root_dir = root_dir
+        #self.root_dir = root_dir
         self.sequence_len = sequence_len
-        self.transform = transform
+        self.network_type = network_type
         self.data_limiter = data_limiter
+        self.transform = transform
 
+        self.sequences = Sequences(root_dir, sequence_len)
         self.lookup = self.__create_lookup()
 
     def __len__(self):
@@ -122,16 +143,46 @@ class FOIKineticPoseDataset(Dataset):
             idx = idx.tolist()
 
         if isinstance(idx, slice):
-            raise NotImplementedError
+            raise TypeError
 
-        seq = Sequence(self.root_dir, self.lookup[idx], self.sequence_len)
-
-        item = {"seq_idx": idx, "key": seq.id, "label": seq.label, "sequence": seq.poses}
-
-        if self.transform:
-            item = self.transform(item)
+        if self.network_type == "siamese":
+            item = self.__get_siamese(idx)
+        elif self.network_type == "triplet":
+            item = self.__get_triplet(idx)
+        else:
+            item = self.__get_single(idx)
 
         return item
+
+    def __get_single(self, idx):
+        sequence, label = self.sequences(self.lookup[idx])
+
+        if self.transform:
+            sequence = self.transform(sequence)
+
+        return sequence, label
+
+    def __get_siamese(self, idx):
+        positive_sequence, positive_label = self.sequences(self.lookup[idx])
+        negative_sequence, _ = self.sequences(self.lookup[idx])
+
+        if self.transform:
+            positive_sequence = self.transform(positive_sequence)
+            negative_sequence = self.transform(negative_sequence)
+
+        return positive_sequence, negative_sequence, positive_label
+
+    def __get_triplet(self, idx):
+        anchor_sequence, anchor_label = self.sequences(self.lookup[idx])
+        positive_sequence, _ = self.sequences(self.lookup[idx])
+        negative_sequence, _ = self.sequences(self.lookup[idx])
+
+        if self.transform:
+            anchor_sequence = self.transform(anchor_sequence)
+            positive_sequence = self.transform(positive_sequence)
+            negative_sequence = self.transform(negative_sequence)
+
+        return anchor_sequence, positive_sequence, negative_sequence, anchor_label
 
     def __create_lookup(self):
         data_info = read_from_json(self.json_path)
