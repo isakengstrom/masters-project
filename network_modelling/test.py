ae432e0e7f8db7f4267a763f597a7fa82643719a
Isak Engström <isaeng@K2006560.win.foi.se>
2021-04-13 16:27:13 +0200

    Add initial test loop

--- network_modelling/test.py
+++ network_modelling/test.py
@@ -1,7 +1,31 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 
-def test(model, testloader, use_cuda):
 
-    return 1, 2, 3
+def test(model, test_loader, device):
+    """"""
+
+    correct = 0
+    total = 0
+
+    model.eval()
+    with torch.no_grad():
+        for (sequence, label) in test_loader:
+
+            label = label.to(device)
+            sequence = sequence.to(device)
+
+            sequence_out = model(sequence)
+
+            _, predicted = torch.max(sequence_out.data, 1)
+            correct += (predicted == label).sum().item()
+
+            total += label.size(0)
+
+    accuracy = 100 * correct / total
+
+    print(f'Accuracy of the network when testing on the test sequences: {accuracy:.3f}')
+
+    return accuracy
+
+
