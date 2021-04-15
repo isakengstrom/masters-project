cca8a6df106a09fc1cc384863a88b83491cee27e
Isak Engström <isaeng@K2006560.win.foi.se>
2021-04-15 15:26:33 +0200

    Add evaluate.py
    - This will be called for validation and test and will replace test.py

---
+++ network_modelling/evaluate.py
@@ -0,0 +1,22 @@
+import torch
+
+
+# https://pytorch.org/tutorials/beginner/text_sentiment_ngrams_tutorial.html
+def evaluate(data_loader, model, device):
+    model.eval()
+
+    total_accuracy, total_count = 0, 0
+
+    with torch.no_grad():
+        for batch_idx, (sequence, label) in enumerate(data_loader):
+            label = label.to(device)
+            sequence = sequence.to(device)
+
+            sequence_out = model(sequence)
+
+            _, predicted_label = torch.max(sequence_out, 1)
+
+            total_accuracy += (predicted_label == label).sum().item()
+            total_count += label.size(0)
+
+    return total_accuracy / total_count
