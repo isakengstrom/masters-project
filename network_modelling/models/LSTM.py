5bffb4d510be69a4c4e8c830c1ef0862903488a2
Isak Engström <isaeng@K2006560.win.foi.se>
2021-05-07 15:40:16 +0200

    Add dropout to all lstm nets

--- network_modelling/models/LSTM.py
+++ network_modelling/models/LSTM.py
@@ -1,19 +1,18 @@
 import torch
 import torch.nn as nn
 
-
 # https://towardsdatascience.com/metric-learning-loss-functions-5b67b3da99a5
 class LSTM(nn.Module):
     """
     First and simplest LSTM net
     """
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, device):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, device, dropout=.5):
         super(LSTM, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.device = device
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
@@ -36,13 +35,13 @@ class LSTM_2(nn.Module):
     """
     Second LSTM net, with a few more layers
     """
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, device):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, device, dropout=.5):
         super(LSTM_2, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.device = device
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
         self.fc_1 = nn.Linear(hidden_size, 128)
         self.fc = nn.Linear(128, num_classes)
         self.relu = nn.ReLU()
@@ -64,13 +63,13 @@ class LSTM_2(nn.Module):
 
 
 class BDLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, device):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, device, dropout=.5):
         super(BDLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.device = device
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=.5, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True, bidirectional=True)
         self.fc = nn.Linear(hidden_size * 2, num_classes)
 
     def forward(self, x):
@@ -90,13 +89,13 @@ class AladdinLSTM(nn.Module):
     Source code:
     https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/Basics/pytorch_rnn_gru_lstm.py#L78
     """
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, device, seq_len=28):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, device, dropout=.5, seq_len=28):
         super(AladdinLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.device = device
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
         self.fc = nn.Linear(hidden_size*seq_len, num_classes)
 
     def forward(self, x):
