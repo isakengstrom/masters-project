0823e0cdae1793e727173b9d3c8d2eee97014b1c
Isak Engström <isaeng@K2006560.win.foi.se>
2021-05-11 14:44:06 +0200

    Update storing of runtime info and lr_lim
    - The lr will step after 10 unincreasing validations the first time instead of 5, the following steps will be at 5 as before.

--- network_modelling/learn.py
+++ network_modelling/learn.py
@@ -7,7 +7,7 @@ from train import train
 from evaluate import evaluate
 
 
-def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs, device, classes, tb_writer,
+def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs, device, classes, tb_writer, lr_lim,
           loss_type="triplet"):
 
     learn_start_time = time.time()
@@ -19,18 +19,17 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=.1)
     curr_lr = optimizer.param_groups[0]['lr']
     prev_lr = curr_lr
-    lim_lr = 5.1e-10
 
     bad_val_counter = 0
-    bad_val_lim = 5
+    bad_val_lim = 10
 
-    learn_info = {'epochs': dict(), 'learning_rate_lim': lim_lr}
+    learn_info = dict()
 
     for epoch_idx in range(1, num_epochs + 1):
         epoch_start_time = time.time()
         epoch_status_message = f"| Epoch {epoch_idx:{epoch_formatter}.0f}/{num_epochs} "
 
-        if curr_lr <= lim_lr:
+        if curr_lr <= lr_lim:
             if bad_val_lim-bad_val_counter <= 0:
                 break
             print(epoch_status_message + f"| Breaking in {bad_val_lim-bad_val_counter} more un-increasing vals |")
@@ -68,11 +67,11 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
 
         print(epoch_status_message +
               f"| Val accuracy: {val_info['accuracy']:.6f} "
-              f"| Duration: {epoch_time:.2f}s |")
-        print('-' * 72)
+              f"| Duration: {epoch_time:.2f}s | ------------")
+        #print('-' * 72)
 
         # Save run info
-        learn_info['epochs'][epoch_idx] = {
+        learn_info[epoch_idx] = {
             'train_info': train_info,
             'val_info': val_info,
             'time': epoch_time,
@@ -87,11 +86,12 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
         if prev_val_acc is not None and prev_val_acc >= val_info['accuracy']:
             bad_val_counter += 1
 
-            if bad_val_counter >= bad_val_lim and curr_lr > lim_lr:
+            if bad_val_counter >= bad_val_lim and curr_lr > lr_lim:
                 # Step the learning rate
                 scheduler.step()
                 curr_lr = optimizer.param_groups[0]['lr']
                 bad_val_counter = 0
+                bad_val_lim = 5
         else:
             prev_val_acc = val_info['accuracy']
 
