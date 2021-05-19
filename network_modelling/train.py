3530ef842d1538a51e19958bec47d68575c1e317
Isak Engström <isaeng@K2006560.win.foi.se>
2021-05-19 09:59:54 +0200

    Debug accuracy during training
    - Try create a way to calculate accuracy during training. Didn't work so well..

--- network_modelling/train.py
+++ network_modelling/train.py
@@ -1,9 +1,11 @@
 import math
 import torch
+import torch.nn as nn
 from collections import Counter
 
 import torch.nn.utils as utils
-from torch.utils.tensorboard import SummaryWriter
+#from torch.utils.tensorboard import SummaryWriter
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
 
 
 def get_all_triplets_indices(labels):
@@ -24,7 +26,7 @@ def get_all_triplets_indices(labels):
 
 #  https://www.kaggle.com/hirotaka0122/triplet-loss-with-pytorch
 def train(data_loader, model, optimizer, loss_function, device, loss_type, epoch_idx, num_epochs, classes, max_norm,
-          tb_writer: SummaryWriter = None):
+          tb_writer):#: SummaryWriter = None):
 
     # Total values are concatenated for the whole epoch.
     total_accuracy, total_count, total_loss = 0, 0, 0
@@ -32,7 +34,7 @@ def train(data_loader, model, optimizer, loss_function, device, loss_type, epoch
 
     num_classes = len(classes)
     num_batches = len(data_loader)
-    log_interval = max(math.floor(num_batches/3), 1)
+    log_interval = max(math.floor(num_batches / 3), 1)
     is_verbose = False
 
     # Used for spacing in the formatting of the status prints
@@ -42,8 +44,8 @@ def train(data_loader, model, optimizer, loss_function, device, loss_type, epoch
     # Tensorboard variables
     global_step = (epoch_idx - 1) * num_batches  # Global step, unique for each combination of epoch and batch index.
     tb_classes = [f"sub{class_idx}" for class_idx in classes]
-    tb_features = torch.zeros(0, num_classes).to(device)
     tb_class_labels = []
+    embeddings = torch.zeros(0, num_classes).to(device)
 
     # Store runtime information
     train_info = {
@@ -118,9 +120,61 @@ def train(data_loader, model, optimizer, loss_function, device, loss_type, epoch
         total_count += labels.size(0)
         total_loss += loss.item()
 
+        """
+        #print(embeddings.size(0))
+        if embeddings.size(0) > 0:
+            print(sequences_out.cpu().detach().numpy().shape)
+            print(embeddings.cpu().detach().numpy().shape)
+            neighbs = neigh.fit(embeddings.cpu().detach().numpy(), sequences_out.cpu().detach().numpy())
+            print(neighbs)
+
+            '''
+            #print('embedding', embeddings.size())
+            #print('seq out', sequences_out.size())
+
+            detached_embeddings = embeddings.detach()
+            detached_sequences_out = sequences_out.detach()
+            n = detached_embeddings.size(0)
+            m = detached_sequences_out.size(0)
+            d = detached_embeddings.size(1)
+
+            detached_embeddings = detached_embeddings.unsqueeze(1).expand(n, m, d)
+            preds = detached_sequences_out.unsqueeze(0).expand(n, m, d)
+
+            #print(detached_embeddings.size())
+            #print(preds.size())
+            dist = torch.pow(detached_embeddings - preds, 2).sum(2)
+            #print(dist)
+
+            knn_indices = dist.topk(k=3, dim=0, largest=False).indices
+
+            print(knn_indices)
+            '''
+            #print(labels)
+            #print(knn_indices.size(0))
+            #tiled_labels = labels.repeat(knn_indices.size(0), 1)
+            #print(tiled_labels)
+            #classified_labels = tiled_labels[:, knn_indices][0]
+            #print(knn_indices)
+            #print(tiled_labels)
+            #print(classified_labels.size())
+
+            #res = torch.bincount(classified_labels)
+            #print(res)
+
+            '''
+            dist = torch.norm(embeddings - sequences_out, dim=1, p=None)
+            print('dist', dist.size())
+            knn = dist.topk(3, largest=False)
+            #print(knn.size())
+            '''
+
+
+        """
+        embeddings = torch.cat((embeddings, sequences_out), 0)
+
         if tb_writer is not None:
             # Store the information for the tensorboard embeddings
-            tb_features = torch.cat((tb_features, sequences_out), 0)
             tb_class_labels.extend([tb_classes[prediction] for prediction in labels])
 
         # Don't want an update of the status or of tensorboard at first batch_idx, therefore continue
@@ -128,14 +182,16 @@ def train(data_loader, model, optimizer, loss_function, device, loss_type, epoch
             continue
 
         if batch_idx % log_interval == 0:
-            curr_accuracy = total_accuracy/total_count
-            curr_loss = total_loss/(batch_idx+1)
+
+            curr_accuracy = total_accuracy / total_count
+            curr_loss = total_loss / (batch_idx + 1)
 
             print(f"| Epoch {epoch_idx:{epoch_formatter}.0f}/{num_epochs} "
-                  f"| Batch {batch_idx+1:{batch_formatter}.0f}/{num_batches} "
-                  f"| Accuracy: {curr_accuracy:.6f} "
-                  f"| Loss: {curr_loss:9.6f} |")
-                  #f"| Global step {global_step} "
+                  f"| Batch {batch_idx + 1:{batch_formatter}.0f}/{num_batches} "
+                  f"| Loss: {curr_loss:9.6f} "
+                  f"{'| Accuracy: {curr_accuracy:.6f}' if loss_type == 'single' else ''} "
+                  # f"| Global step {global_step} "
+                  )
 
             if tb_writer is not None:
                 # Add scalars to Tensorboard
@@ -157,7 +213,7 @@ def train(data_loader, model, optimizer, loss_function, device, loss_type, epoch
 
     if tb_writer is not None:
         # Add embeddings to tensorboard and flush everything in the writer to disk
-        tb_writer.add_embedding(tb_features, metadata=tb_class_labels, global_step=global_step)
+        tb_writer.add_embedding(embeddings, metadata=tb_class_labels, global_step=global_step)
         tb_writer.flush()
 
     return model, train_info
