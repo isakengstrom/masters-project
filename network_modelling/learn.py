6cd5cfa49764ac588d8cefa73bd5bf8f9f3eb510
Isak Engström <isaeng@K2006560.win.foi.se>
2021-05-21 11:50:36 +0200

    Adapt learn cycle to possible metric evaluation

--- network_modelling/learn.py
+++ network_modelling/learn.py
@@ -4,11 +4,11 @@ import math
 import torch.optim.lr_scheduler
 
 from train import train
-from evaluate import evaluate
+from evaluate import evaluate, evaluate_metric
 
 
 def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs, device, classes, lr_lim, loss_type,
-          max_norm, tb_writer):
+          task, max_norm, step_size, tb_writer):
 
     learn_start_time = time.time()
 
@@ -16,12 +16,12 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
 
     epoch_formatter = int(math.log10(num_epochs)) + 1
 
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=.1)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=.1)
     curr_lr = optimizer.param_groups[0]['lr']
     prev_lr = curr_lr
 
     bad_val_counter = 0
-    bad_val_lim = 10
+    bad_val_lim = 30
 
     learn_info = dict()
 
@@ -29,15 +29,16 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
         epoch_start_time = time.time()
         epoch_status_message = f"| Epoch {epoch_idx:{epoch_formatter}.0f}/{num_epochs} "
 
-        if curr_lr <= lr_lim:
-            if bad_val_lim-bad_val_counter <= 0:
-                break
-            print(epoch_status_message + f"| Breaking in {bad_val_lim-bad_val_counter} more un-increasing vals |")
-        elif prev_lr == curr_lr:
-            print(epoch_status_message + f"| LR: {curr_lr:g} "
-                                         f"| Step after {bad_val_lim-bad_val_counter} more un-increasing vals |")
-        else:
-            print(epoch_status_message + f"| Stepped the LR to: {curr_lr:g}")
+        if lr_lim is not None:
+            if curr_lr <= lr_lim:
+                if bad_val_lim-bad_val_counter <= 0:
+                    break
+                print(epoch_status_message + f"| Breaking in {bad_val_lim-bad_val_counter} more un-increasing vals |")
+            elif prev_lr == curr_lr:
+                print(epoch_status_message + f"| LR: {curr_lr:g} "
+                                             f"| Step after {bad_val_lim-bad_val_counter} more un-increasing vals |")
+            else:
+                print(epoch_status_message + f"| Stepped the LR to: {curr_lr:g}")
 
         prev_lr = curr_lr
 
@@ -49,6 +50,7 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
             loss_function=loss_function,
             device=device,
             loss_type=loss_type,
+            task=task,
             epoch_idx=epoch_idx,
             num_epochs=num_epochs,
             classes=classes,
@@ -57,19 +59,37 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
         )
 
         # Validate the epoch
-        val_info = evaluate(
-            data_loader=val_loader,
-            model=model,
-            device=device,
-            is_test=False
-        )
+        if task == 'classification':
+            val_info = evaluate(
+                data_loader=val_loader,
+                model=model,
+                device=device,
+                is_test=False
+            )
+
+            epoch_status_message += f"| Val accuracy: {val_info['accuracy']:.6f} "
+
+        elif task == 'metric':
+            val_info = evaluate_metric(
+                train_loader=train_loader,
+                eval_loader=val_loader,
+                model=model,
+                device=device,
+                classes=classes,
+                is_test=False
+            )
+
+            epoch_status_message += f"| Pre@1: {val_info['precision_at_1']:.6f} " \
+                                    f"| R-Pre: {val_info['r_precision']:.6f} " \
+                                    f"| MAP@R: {val_info['mean_average_precision_at_r']:.6f} " \
+                                    f"| Sil: {val_info['silhouette']:.3f} " \
+                                    f"| CH: {val_info['ch']:.0f} "
+        else:
+            raise Exception("Invalid task type, should either by 'classification' or 'metric'")
 
         epoch_time = time.time() - epoch_start_time
 
-        print(epoch_status_message +
-              f"| Val accuracy: {val_info['accuracy']:.6f} "
-              f"| Duration: {epoch_time:.2f}s | ------------")
-        #print('-' * 72)
+        print(epoch_status_message + f"| Duration: {epoch_time:.2f}s | ------------")
 
         # Save run info
         learn_info[epoch_idx] = {
@@ -84,17 +104,20 @@ def learn(train_loader, val_loader, model, optimizer, loss_function, num_epochs,
         file_path = f'./saves/checkpoints/checkpoint_{epoch_idx}.pth'
         torch.save(state, file_path)
 
-        if prev_val_acc is not None and prev_val_acc >= val_info['accuracy']:
-            bad_val_counter += 1
+        if lr_lim is not None:
+            if prev_val_acc is not None and prev_val_acc >= val_info['accuracy']:
+                bad_val_counter += 1
 
-            if bad_val_counter >= bad_val_lim and curr_lr > lr_lim:
-                # Step the learning rate
-                scheduler.step()
-                curr_lr = optimizer.param_groups[0]['lr']
-                bad_val_counter = 0
-                bad_val_lim = 5
+                if bad_val_counter >= bad_val_lim and curr_lr > lr_lim:
+                    # Step the learning rate
+                    scheduler.step()
+                    curr_lr = optimizer.param_groups[0]['lr']
+                    bad_val_counter = 0
+                    bad_val_lim = 5
+            else:
+                prev_val_acc = val_info['accuracy']
         else:
-            prev_val_acc = val_info['accuracy']
+            scheduler.step()
 
     print(f'| Finished learning | Learning time: {(time.time()-learn_start_time):.2f}s')
 
